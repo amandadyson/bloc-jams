8e28b0effa23ecfa4f36c092772f9470d77bfdaa
Amanda <amandadyson@users.noreply.github.com>
2017-08-14 16:44:07 -0700

    Complete Checkpoint 21 Assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,24 @@
 var animatePoints = function() {
 
+  var points = document.getElementsByClassName('point');
+
+  var revealPoint = function(index) {
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+  for (var i = 0; i < points.length ; i++) {
+    revealPoint(i);
+  }
+};
+
+animatePoints();
+
+/*
+var animatePoints = function() {
+
 var points = document.getElementsByClassName('point');
 
 var revealFirstPoint = function() {
@@ -28,5 +47,4 @@ revealSecondPoint();
 revealThirdPoint();
 
 };
-
-console.log(animatePoints());
+*/
