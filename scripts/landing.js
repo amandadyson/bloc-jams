f2ec7011cc6ee30f8829a69d9dad97d5b6eb1ea8
Amanda <amandadyson@users.noreply.github.com>
2017-08-14 18:07:44 -0700

    Complete checkpoint 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-  var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
   var revealPoint = function(index) {
       points[index].style.opacity = 1;
@@ -14,7 +14,22 @@ var animatePoints = function() {
   }
 };
 
-animatePoints();
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200; //scroll at least 200 px into the .selling-points element
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+    }
+  });
+};
 
 /*
 var animatePoints = function() {
