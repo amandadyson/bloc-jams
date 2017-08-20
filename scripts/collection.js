6a6628fe7f4e453eb7969360e2807d3c11951666
Amanda <amandadyson@users.noreply.github.com>
2017-08-19 20:03:00 -0700

    correct collection code from checkpoint 23

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,3 +1,20 @@
+var collectionItemTemplate =
+    '<div class="collection-album-container column fourth">'
+  + '  <img src="assets/images/album_covers/01.png"/>'
+  + '  <div class="collection-album-info caption">'
+  + '    <p>'
+  + '      <a class="album-name" href="album.html"> The Colors </a>'
+  + '      <br/>'
+  + '      <a href="album.html"> Pablo Picasso </a>'
+  + '      <br/>'
+  + '      X songs'
+  + '      <br/>'
+  + '    </p>'
+  + '  </div>'
+  + '</div>'
+  ;
+
+
 window.onload = function() {
 
     // select first element with album-covers class name and assign it to a variable named collectionContainer
