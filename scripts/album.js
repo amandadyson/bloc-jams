d07fb8412ae5e46a629eb9bc02781d4c926003b2
Amanda <amandadyson@users.noreply.github.com>
2017-08-19 19:39:29 -0700

    complete checkpoint 24 assignment

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,21 @@ var albumMarconi = {
     ]
 };
 
+// A third example album
+var albumFogust = {
+    title: 'Fogust',
+    artist: 'San Francisco',
+    label: 'Fog City',
+    year: '1776',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Fogust', duration: '1:01' },
+        { title: 'Carl the Fog', duration: '5:01' },
+        { title: 'Fog Rolling In', duration: '3:21'},
+        { title: 'Cold Summer', duration: '3:14' },
+    ]
+};
+
 //createSongRow functions assigns song row template to a variable named template and returns it; function takes song number, name, length as arguments and populates the song row template accordingly
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
@@ -43,14 +58,16 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+//moved this to the global scope
+// select all html elements required to display: title, artist, release info, image, and song list; in order to populate these with info we assign corresponding values of the album objects' properties to the html elements
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 //function that the program calls when the window loads; will utilize the object's stored info by injecting it into the template
 var setCurrentAlbum = function(album) {
-    // select all html elements required to display: title, artist, release info, image, and song list; in order to populate these with info we assign corresponding values of the album objects' properties to the html elements
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -68,4 +85,18 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+//gives easy access to using array index numbers
+var albums = [albumPicasso, albumMarconi, albumFogust];
+
+//sets the default value; here, it's the first one that will display on the click event; needs to be declared outside of the function so that when it increases by one that state is remembered and carries over to the next click event
+var index = 1;
+
+albumImage.addEventListener('click', function(event) {
+    setCurrentAlbum(albums[index]);
+    index++; //add 1 with each click to the index variable; the order of this is important
+    if (index == albums.length) { //reset once we reach the end of all of the albums
+      index = 0;
+    }
+  });
 };
