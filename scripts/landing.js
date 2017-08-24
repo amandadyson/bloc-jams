f71067f49c827175a2293b8e78f9f5a06a378d4d
Amanda <amandadyson@users.noreply.github.com>
2017-08-23 22:04:28 -0700

    complete checkpoint 28

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,3 +1,33 @@
+var animatePoints = function() {
+
+
+var revealPoint = function() {
+     $(this).css({
+         opacity: 1,
+         transform: 'scaleX(1) translateY(0)'
+      });
+    };
+
+    $.each($('.point'), revealPoint);
+};
+
+$(window).load(function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+  if ($(window).height() > 950) {
+      animatePoints();
+  }
+
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+$(window).scroll(function(event) {
+  if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
+    }
+  });
+});
+
+/* vanilla javascript
+
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
@@ -30,6 +60,7 @@ window.onload = function() {
     }
   });
 };
+*/
 
 /*
 var animatePoints = function() {
